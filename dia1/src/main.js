ade7e805fe91fce0096be7dbc50d883e77f11069
Rapaix <rafaelpx8812@gmail.com>
2021-08-17 11:23:41 -0300

    solucionado

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,19 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector('[data-js="app"]')
+const link = document.querySelector('[data-js="link"]')
+
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+link.addEventListener('click', (event) => {
+
+  event.preventDefault()
+  if (app.style.visibility !== 'hidden') {
+    app.style.visibility = "hidden"
+  } else {
+    app.style.visibility = "visible"
+  }
+
+}, false)
